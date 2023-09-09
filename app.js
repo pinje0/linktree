88cb87379b2b794729b7cd64642ccf234491ad01
pinje0 <melvinaustinn@gmail.com>
2023-09-09 17:49:37 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const shareButtons = document.querySelectorAll(".tile-share-button");
+const headerShareButton = document.querySelectorAll(".share-button");
+
+async function copyText(e) {
+    // prevent button going to the site
+    e.preventDefault();
+    const link = this.getAttribute("link");
+    console.log(link);
+    try {
+        await navigator.clipboard.writeText(link);
+        alert(`Copied the text: ${link}`);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+shareButtons.forEach((shareButtons) =>
+    shareButtons.addEventListener("click", copyText)
+);
+headerShareButton.forEach((headerShareButton) =>
+    headerShareButton.addEventListener("click", copyText)
+);
