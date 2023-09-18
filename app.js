99d4b4a3e1e44372b2b917554c39700d29f3ce70
pinje0 <melvinaustinn@gmail.com>
2023-09-18 15:25:07 +0700

    add SweetAlert2 JS library

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-const shareButtons = document.querySelectorAll(".tile-share-button");
-const headerShareButton = document.querySelectorAll(".share-button");
+const shareButtons = document.querySelectorAll('.tile-share-button');
+const headerShareButton = document.querySelectorAll('.share-button');
 
 async function copyText(e) {
     // prevent button going to the site
     e.preventDefault();
-    const link = this.getAttribute("link");
+    const link = this.getAttribute('link');
     console.log(link);
     try {
         await navigator.clipboard.writeText(link);
         // alert(`Copied the text: ${link}`);
-        alert(`📋Copied to Clipboard`);
+        // alert(`📋Copied to Clipboard`);
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: '📋Copied to Clipboard',
+            showConfirmButton: false,
+            timer: 800,
+            width: 200,
+            height: 100,
+            padding: '1em',
+        });
     } catch (err) {
         console.error(err);
     }
 }
 
 shareButtons.forEach((shareButtons) =>
-    shareButtons.addEventListener("click", copyText)
+    shareButtons.addEventListener('click', copyText)
 );
 headerShareButton.forEach((headerShareButton) =>
-    headerShareButton.addEventListener("click", copyText)
+    headerShareButton.addEventListener('click', copyText)
 );
