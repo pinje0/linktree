8b475b37b9ae224d8530bc1686789c888df51110
pinje0 <melvinaustinn@gmail.com>
2023-09-30 12:49:55 +0700

    disable highlight text and drag element & refactoring code

--- app.js
+++ app.js
@@ -5,20 +5,8 @@ async function copyText(e) {
     // prevent button going to the site
     e.preventDefault();
     const link = this.getAttribute('link');
-    console.log(link);
     try {
         await navigator.clipboard.writeText(link);
-        // alert(`Copied the text: ${link}`);
-        // alert(`📋Copied to Clipboard`);
-        // Swal.fire({
-        //     position: 'top-end',
-        //     title: '📋Copied to Clipboard',
-        //     showConfirmButton: false,
-        //     timer: 550,
-        //     width: '150px',
-        //     height: '50px',
-        //     padding: '1em',
-        // });
         Snackbar.show({
             text: '📋Copied to Clipboard',
             pos: 'bottom-right',
